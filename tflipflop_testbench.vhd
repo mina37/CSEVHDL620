cdfdde337dd6359255cf6a4977f6ab6356df92ed
mina37 <minasherif7@hotmail.com>
2019-01-04 12:16:56 +0200

    tflipflop test

--- tflipflop_testbench.vhd
+++ tflipflop_testbench.vhd
@@ -12,31 +12,64 @@ COMPONENT t_ff IS
 			q: OUT std_logic);
 END COMPONENT t_ff;
 FOR ALL: t_ff USE ENTITY work.t_ff(async_clear);
-SIGNAL t : bit :='0'; 
-SIGNAL clk: bit :='0'; 
-SIGNAL clear: bit :='0';
-SIGNAL q: bit :='0';
+SIGNAL t : std_logic :='0'; 
+SIGNAL clk: std_logic :='0'; 
+SIGNAL clear: std_logic :='0';
+SIGNAL q: std_logic :='0';
 BEGIN  
 	dut: ENTITY work.t_ff(async_clear) PORT MAP (t=>t,clk=>clk,clear =>clear,q=>q);
 	p: PROCESS IS								 
 	BEGIN
-		ip <= '1';
-		clock <= '0'; WAIT FOR 10 ns;
-		clock <= '1'; WAIT FOR 10 ns;
-		ASSERT op = '1' and nop = '0'
+		t <= '0';
+		clk <= '0';
+		clear <= '0'; WAIT FOR 10 ns;
+		clear <= '1'; WAIT FOR 10 ns;
+		ASSERT q = '0'
 		REPORT "ERROR 1!" 
 		SEVERITY error;
-		ip <= '0'; WAIT FOR 10 ns;
-		ASSERT op = '0' and nop = '1'
+		t <= '1'; WAIT FOR 10 ns;
+		ASSERT q = '0'
 		REPORT "ERROR 2!"
 		SEVERITY error;
-        clock <= '0'; WAIT FOR 10 ns;
-        ASSERT op = '0' and nop = '1'
+        clear <= '0'; WAIT FOR 10 ns;
+        ASSERT q = '0'
 		REPORT "ERROR 3!"
 		SEVERITY error;
-		ip <= '1'; WAIT FOR 10 ns;
-        ASSERT op = '0' and nop = '1'
+		clk <= '1'; WAIT FOR 10 ns;
+        ASSERT q = '1'
 		REPORT "ERROR 4!"
+		SEVERITY error;
+		clear <= '1'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 5!"
+		SEVERITY error;
+		t <= '0'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 6!"
+		SEVERITY error;
+		clear <= '0'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 7!"
+		SEVERITY error;
+		clk <= '0'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 8!"
+		SEVERITY error;
+		t <= '1'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 9!"
+		SEVERITY error;
+		clk <= '1'; WAIT FOR 10 ns;
+        ASSERT q = '1'
+		REPORT "ERROR 10!"
+		SEVERITY error;
+		clk <= '0'; WAIT FOR 10 ns;
+        ASSERT q = '1'
+		REPORT "ERROR 11!"
+		SEVERITY error;
+		clk <= '1'; WAIT FOR 10 ns;
+        ASSERT q = '0'
+		REPORT "ERROR 12!"
 		SEVERITY error;
 	
 		WAIT;
